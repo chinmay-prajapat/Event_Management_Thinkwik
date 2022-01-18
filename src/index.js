d7d1fc21f36bbce0ad26a7f5e7859ab5e391314a
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-18 14:39:13 +0530

    middleware for maintenance added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ const eventRouter = require("./routers/event");
 const app = express();
 const PORT = process.env.PORT;
 
+/*  -------- To stop all the transcation ---------*/
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Site is currently down. check back soon");
+// });
+
+/* --------To stop transcation of specific method or methods-------*/
+
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.status(405).send("The service is disabled temporarily");
+//   } else {
+//     next();
+//   }
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(eventRouter);
@@ -12,7 +28,7 @@ app.use(eventRouter);
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
 });
-const bcrypt = require("bcryptjs");
+
 const jwt = require("jsonwebtoken");
 const myFunction = async () => {
   const token = jwt.sign({ _id: "abc" }, "thisismynewcourse", {
