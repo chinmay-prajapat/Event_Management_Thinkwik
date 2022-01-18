7797f8431db9d8dcbdea2026f3e7563111228a93
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-18 13:03:55 +0530

    login system

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,4 @@
+const bcryptjs = require("bcryptjs");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
@@ -49,6 +50,19 @@ const userSchema = new mongoose.Schema({
     type: Buffer,
   },
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+  return user;
+};
+
 userSchema.pre("save", async function (next) {
   const user = this;
   if (user.isModified("password")) {
